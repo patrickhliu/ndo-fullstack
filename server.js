ce6c369fc5bbf30f721c4d63575826fc4c2380b2
Patrick Liu <patrick.h.liu@gmail.com>
2025-10-16 10:40:10 -0700

    edit

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const reactPath = path.join(__dirname, 'client/dist');
 console.log(reactPath);
 app.use(express.static(reactPath));
 
+app.use('/nintendo', routes);
+
 app.get('*all', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
