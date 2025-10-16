43e9db86784a549d409b0d6e91565621d7983bf9
Patrick Liu <patrick.h.liu@gmail.com>
2025-10-15 20:40:24 -0700

    add db connections

---
+++ server/config/db.js
@@ -0,0 +1,21 @@
+import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config()
+
+const sequelize = new Sequelize (
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    }
+);
+
+sequelize.authenticate().then(() => {
+    console.log("DATABASE CONNECTED");
+}).catch((err) => {
+    console.log(err);
+});
+
+export default sequelize;
--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
+import sequelize from './config/db.js';
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const port = 8080;
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -5,12 +10,13 @@ const corsOptions = {
     origin: "http://localhost:5173",
 };
 
+
 app.use(cors(corsOptions));
 
 app.get("/api", (req, res) => {
     res.json({ fruits: ["apple", "orange"] });
 });
 
-app.listen(8080, () => {
-    console.log("server started on 8080");
+app.listen(port, () => {
+  console.log(`SERVER RUNNING ON PORT ${port}`);
 });
